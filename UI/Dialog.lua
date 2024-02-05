bec788bf76ae7184989abda3053499f78ba1242c
teelolws <49792789+teelolws@users.noreply.github.com>
2024-02-06 11:17:03 +1300

    Update Dialog.lua

--- UI/Dialog.lua
+++ UI/Dialog.lua
@@ -1,23 +1,3 @@
--------------------------------------------------------------------------------
--- Premade Groups Filter
--------------------------------------------------------------------------------
--- Copyright (C) 2022 Elotheon-Arthas-EU
---
--- This program is free software; you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation; either version 2 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License along
--- with this program; if not, write to the Free Software Foundation, Inc.,
--- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
--------------------------------------------------------------------------------
-
 local PGF = select(2, ...)
 local L = PGF.L
 local C = PGF.C
@@ -73,6 +53,7 @@ function PGFDialog:OnHide()
     if self.activePanel and self.activePanel.OnHide then
         self.activePanel:OnHide()
     end
+    self:ResetPosition(PremadeGroupsFilterSettings.dialogMovable)
 end
 
 function PGFDialog:OnMouseDown(button)
@@ -233,8 +214,9 @@ function PGFDialog:ResetPosition(keepParentOffset)
     if keepParentOffset then
         local xp, yp, wp, hp = self:GetParent():GetRect()
         local xs, ys, ws, hs = self:GetRect()
-        x = xs - xp - wp
-        y = ys - yp
+        local scale = self:GetScale()
+        x = xs - (xp/scale) - (wp/scale)
+        y = ys - (yp/scale) - (hp/scale) + hp
     end
     if not PGF.SupportsDragonflightUI() then
         x = x - 5
